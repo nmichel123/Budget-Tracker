8ba443e44d45987a3ed6b15a5ee3303ef6c34963
Nico Michel <nicomichel@utexas.edu>
2020-08-20 13:38:16 -0500

    added process env mongodb url

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+mongoose.connect(MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
